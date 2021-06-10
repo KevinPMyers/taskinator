1dab5e650201c78fc20e07ab6684d8eef587b4c7
KevinPMyers <kmyers25@gmail.com>
2021-06-10 15:25:30 -0400

    added back console.dir, somehow got deleted along the way

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,8 @@ var createTaskEl = function(taskDataObj) {
 
   listItemEl.appendChild(taskInfoEl);
 
+  console.dir(listItemEl);
+
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 }
